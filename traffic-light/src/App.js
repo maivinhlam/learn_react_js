d0c7b853bec85a452f7ae8f83344687de2429ee4
lam-mv <lam.mv@terra-drone.co.jp>
2022-06-12 20:40:15 +0700

    [Add] State

--- traffic-light/src/App.js
+++ traffic-light/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import TrafficLight  from './components/TrafficLight';
 function App() {
--- traffic-light/src/components/TrafficLight.js
+++ traffic-light/src/components/TrafficLight.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./TrafficLight.css";
-
+import classNames from 'classnames'
 const RED = 0;
 const ORANGE = 1;
 const GREEN = 2;
@@ -8,28 +8,41 @@ const GREEN = 2;
 class TrafficLight extends Component {
   constructor() {
     super();
-    this.currentColor = RED;
+    this.state = {
+		currentColor: RED
+	}
+	
+	setInterval(() => {
+		this.setState({
+			currentColor: this.getNextColor(this.state.currentColor)
+		})
+	}, 1000);
   }
 
   getNextColor(color) {
     switch (color) {
       case RED:
         return ORANGE;
-        break;
       case ORANGE:
         return GREEN;
-        break;
       default:
         return RED;
     }
   }
   
   render() {
+	const {currentColor} = this.state
 	return (
 		<div className="TrafficLight">
-			<div className="bulb red"/>
-			<div className="bulb orange"/>
-			<div className="bulb green"/>
+			<div className={classNames('bulb', 'red', {
+				active: currentColor=== RED
+			})}/>
+			<div className={classNames('bulb', 'orange', {
+				active: currentColor=== ORANGE
+			})}/>
+			<div className={classNames('bulb', 'green', {
+				active: currentColor=== GREEN
+			})}/>
 		</div>
 	)
   }
