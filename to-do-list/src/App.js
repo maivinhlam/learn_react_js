eeb316c13fc1e56e743268f7439334fe7349849f
lam-mv <lam.mv@terra-drone.co.jp>
2022-06-12 16:15:01 +0700

    [Add] component

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+import ToDoItem from './components/ToDoItem';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <ToDoItem/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ to-do-list/src/components/ToDoItem.js
@@ -0,0 +1,15 @@
+import React,{ Component } from "react";
+
+class ToDoItem extends Component {
+	render() {
+		return (
+			<div className="ToDoItem">
+				<p>Di Cho</p>
+				<p>Di Cho 2</p>
+				<p>Di Cho 3</p>
+			</div>
+		);
+	}
+}
+
+export default ToDoItem;
