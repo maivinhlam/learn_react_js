391524ddf9b4aeb05b29e18ed70b4d33ddb21e26
lam-mv <lam.mv@terra-drone.co.jp>
2022-10-28 23:32:59 +0700

    [Add] Add bootstrap

--- README.md
+++ README.md
@@ -5,4 +5,7 @@
     npx create-react-app todo-list --template typescript
 `
 
-##
+## Add bootstrap
+`
+    npm install react-bootstrap bootstrap
+`
--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Button >Show button</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
