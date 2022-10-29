d1b0c7d12bfe1512cf4e5477658c76ae9e0b3ce1
lam-mv <lam.mv@terra-drone.co.jp>
2022-10-30 00:42:07 +0700

    [Add] Apply Redux for UI

--- todo-list/src/components/item.tsx
+++ todo-list/src/components/item.tsx
@@ -1,16 +1,42 @@
+import React from "react";
 import { Button, Stack } from "react-bootstrap";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashCan, faSquareCheck, faSquare } from '@fortawesome/free-regular-svg-icons'
+import { Todo } from "../features/todo-list/slice"
+import { deleteTodo, editTodo } from '../features/todo-list/slice';
+import { useAppDispatch } from '../store'
 
-function Item() {
+interface Props {
+  todo: Todo;
+}
+const Item = React.memo(({ todo }: Props) => {
+  const dispatch = useAppDispatch();
+
+  const setDone = () => {
+    dispatch(editTodo({
+      id: todo.id, value: todo.value, isDone: !todo.isDone
+    }))
+  }
+  const handleDelete = () => {
+    dispatch(deleteTodo({ id: todo.id }))
+  }
   return (
     <Stack direction="horizontal" gap={4}>
-      <input type="checkbox" id="topping" name="topping" value="Paneer" /> 
-      <FontAwesomeIcon icon={['fas', 'square']} className='text-danger' />
-      
-      <text> item 2</text>
-      <Button variant="outline-secondary" className="ms-auto"><FontAwesomeIcon icon={['far', 'trash-can']} className='text-danger' /></Button>
+      <input type="checkbox" id="topping" name="topping" value="Paneer" aria-label="check" />
+      {todo.isDone
+        ? <FontAwesomeIcon icon={faSquareCheck} className='text-success' onClick={setDone} id={todo.id} />
+        : <FontAwesomeIcon icon={faSquare} className='text-danger' onClick={setDone} id={todo.id} />
+      }
+      {todo.isDone
+        ? <p className="text-decoration-line-through mb-0"> {todo.value}</p>
+        : <p className="text-decoration-none align-middle mb-0"> {todo.value}</p>
+      }
+
+      <Button variant="outline-secondary" className="ms-auto z-index-3" onClick={handleDelete} id={todo.id}>
+        <FontAwesomeIcon icon={faTrashCan} className='text-danger z-index-1' />
+      </Button>
     </Stack>
   );
-}
+});
 
 export default Item;
--- todo-list/src/components/list.tsx
+++ todo-list/src/components/list.tsx
@@ -2,20 +2,24 @@ import React from "react";
 import "./item";
 import Item from "./item";
 import { Card, ListGroup } from "react-bootstrap";
+import { useAppSelector } from '../store'
 
-function List() {
+const List = React.memo(() => {
+  const listTodo = useAppSelector((state) => state.TodoList.TodoList);
   return (
     <Card className="p-0">
       <Card.Header as="h5">Todo List</Card.Header>
-      <Card.Body>
+      <Card.Body className="p-0">
         <ListGroup variant="flush">
-          <ListGroup.Item>
-            <Item />
-          </ListGroup.Item>
+          {listTodo.map((item: any, index: Number) => (
+            <ListGroup.Item key={String(index)}>
+              <Item todo={item} />
+            </ListGroup.Item>
+          ))}
         </ListGroup>
       </Card.Body>
     </Card>
   );
-}
+});
 
 export default List;
